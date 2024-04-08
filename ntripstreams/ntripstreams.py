1dcb35fec1a2cd3ead009e160f9976cbb4fe1e3b
MRK-DTU <martin97ras+3@gmail.com>
2024-04-08 15:25:41 +0200

    revert

--- ntripstreams/ntripstreams.py
+++ ntripstreams/ntripstreams.py
@@ -527,7 +527,7 @@ class NtripStream:
                 else:
                     self.rtcmFrameBuffer = BitStream()
             if self.rtcmFramePreample and self.rtcmFrameBuffer.length >= 48:
-                self.rtcmFrameBuffer.pos = 0
+                #self.rtcmFrameBuffer.pos = 0
                 (rtcmPreAmple, rtcmPayloadLength) = self.rtcmFrameBuffer.peeklist(
                     rtcm3FrameHeaderFormat
                 )

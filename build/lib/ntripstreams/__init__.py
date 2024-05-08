4faafdc512c0ec67d3e6854bf86292a644e7ec4d
MRK-DTU <martin97ras+3@gmail.com>
2024-05-08 15:02:15 +0200

    Remove .egg-info and build directories

--- build/lib/ntripstreams/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["NtripStream", "Rtcm3", "crc24q", "crcNmea"]
-
-from ntripstreams.crc import crc24q, crcNmea
-from ntripstreams.ntripstreams import NtripStream
-from ntripstreams.rtcm3 import Rtcm3
